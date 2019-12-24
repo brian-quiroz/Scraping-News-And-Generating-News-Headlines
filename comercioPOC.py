d67c745ad14ba061384fcfe27aa54c4330664128
Brian Quiroz <r229q057@ku.edu>
2019-12-24 00:44:22 -0500

    changed dict of lists to dict of dicts

--- comercioPOC.py
+++ comercioPOC.py
@@ -1,39 +1,30 @@
 # importing packages
+from globals import months
 import numpy as np
 import json
 import requests
 from bs4 import BeautifulSoup
 
 def poc(person):
-    class article:
-        __title = ""
-        __link = ""
-        __description = ""
-        __date = ""
-        __diary = ""
-        __body = ""
+    articleData = []
+    scrape(person, "elcomercio", 2, articleData);
+    scrape(person, "peru21", 1, articleData);
+    scrape(person, "publimetro", 1, articleData);
+    scrape(person, "gestion", 1, articleData);
+    scrape(person, "depor", 1, articleData);
+    scrape(person, "trome", 1, articleData);
 
-    dataDict = {"Titles": [], "Links": [], "Descriptions": [], "Dates": [], "Diaries": [], "ArticleBodies": []}
+    return articleData;
 
-    months = {"01": "enero", "02": "febrero", "03": "marzo", "04": "abril", "05": "mayo", "06": "junio", "07": "juio", "08": "agosto", "09": "setiembre", "10": "octubre", "11": "noviembre", "12": "diciembre"}
+def scrape(person, site, ld_index, articleData):
 
-    scrape(person, "elcomercio", 2, dataDict, months);
-    scrape(person, "peru21", 1, dataDict, months);
-    scrape(person, "publimetro", 1, dataDict, months);
-    scrape(person, "gestion", 1, dataDict, months);
-    scrape(person, "depor", 1, dataDict, months);
-    scrape(person, "trome", 1, dataDict, months);
-
-    return dataDict;
-
-def scrape(person, site, ld_index, dataDict, months):
     if (site == "depor"):
         site += ".com"
     else:
         site += ".pe"
 
-    first_name = person.split()[0];
-    last_name = person.split()[1];
+    first_name = person.split()[0]
+    last_name = person.split()[1]
 
     i = 1
     while (i <= 1):
@@ -47,43 +38,44 @@ def scrape(person, site, ld_index, dataDict, months):
 
         print("Entrando a página " + str(i) + " de http://" + site)
         number_of_articles = 0
-        n_start = len(dataDict["Titles"])
+        n_start = len(articleData)
 
         for title in coverpage_news:
             title_text = title.get_text()
             if ((first_name + " " + last_name) in title_text):
-                dataDict["Titles"].append(title_text)
-                dataDict["Links"].append("http://" + site + title.find('a')['href'])
+                newDict = {}
+                newDict["title"] = title_text
+                newDict["link"] = "http://" + site + title.find('a')['href']
+                articleData.append(newDict)
                 number_of_articles += 1
 
-        for n in np.arange(n_start, len(dataDict["Titles"])):
-            article = requests.get(dataDict["Links"][n])
-            article_content = article.content
+        for n in np.arange(n_start, len(articleData)):
+            articleReq = requests.get(articleData[n]["link"])
+            article_content = articleReq.content
             soup_article = BeautifulSoup(article_content, 'html5lib')
             jsonData = json.loads(soup_article.find_all('script', type='application/ld+json')[ld_index].text, strict=False)
 
             description = jsonData["description"]
-            dataDict["Descriptions"].append(description)
+            articleData[n]["description"] = description
 
             date = jsonData["datePublished"]
             year = date[0:4]
             month = months.get(date[5:7])
             day = date[8:10]
             dateFinal = day + " de " + month + " de " + year
-            dataDict["Dates"].append(dateFinal)
+            articleData[n]["date"] = dateFinal
 
             diary = jsonData["publisher"]["name"]
-            dataDict["Diaries"].append(diary)
+            articleData[n]["diary"] = diary
 
             articleBody = jsonData["articleBody"]
-            dataDict["ArticleBodies"].append(articleBody)
+            articleData[n]["body"] = articleBody
 
         n_prev = n + 1
 
         i += 1
 
-    # for key, val in dataDict.items():
-    #     for n in reversed(np.arange(0, len(val))):
-    #         print(str(n) + ": " + val[n])
+    # for i in range(len(articleData)):
+    #     print(articleData[i]["title"])
 
 # poc("cena navideña")
---
+++ globals.py
@@ -0,0 +1,1 @@
+months = {"01": "enero", "02": "febrero", "03": "marzo", "04": "abril", "05": "mayo", "06": "junio", "07": "juio", "08": "agosto", "09": "setiembre", "10": "octubre", "11": "noviembre", "12": "diciembre"}
